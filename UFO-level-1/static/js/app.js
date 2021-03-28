afa298c97baab2acd72e49c7bc4935a84687224b
JLGit-01 <Jlaka019@gmail.com>
2021-03-28 18:55:35 -0500

    v3

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,42 +4,50 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Console.log the weather data from data.js
-console.log(data);
+//console.log(tableData);
 
-data.forEach((alienReport) => {
+tableData.forEach((alienReport) => {
+   
     var row = tbody.append("tr");
     Object.entries(alienReport).forEach(([key, value]) => {
-      var cell = row.append("td"); //table cell
-      cell.text(value);
+        var cell = row.append("td"); //table cell
+        cell.text(value);
     });
-  });
+});
 
 // YOUR CODE HERE!
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Select the form
-var form = d3.select("#form");
+//var form = d3.select("#form");
 
 // Create event handlers 
-button.on("click", runEnter);
-form.on("submit",runEnter);
+button.on("click", function() {
+   
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
-// Complete the event handler function for the form
-function runEnter() {
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    console.log(inputElement)
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+    console.log(inputValue);
+    //console.log(tableData);
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+    //   var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    var filteredData = tableData.filter(x => x.datetime === inputValue)
 
-  console.log(inputValue);
-  console.log(tableData);
+    console.log(filteredData);
+    tbody.html("")
+    filteredData.forEach((alienReport) => {       
+        var row = tbody.append("tr");
+        Object.entries(alienReport).forEach(([key, value]) => {
+            var cell = row.append("td"); //table cell
+            cell.text(value);
+        });
+    });
 
-  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-
-  console.log(filteredData);}
+})
